f3b6a08af2910ef6af09b5c2375339354b7919a6
Sergey Khabibullin <sergey@khabibullin.com>
2018-11-18 21:38:00 +0300

    Add welcome text

---
+++ src/Tel.Egram.Gui/Views/Authentication/WelcomeControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Tel.Egram.Components.Authentication;
+
+namespace Tel.Egram.Gui.Views.Authentication
+{
+    public class WelcomeControl : UserControl
+    {
+        public WelcomeControl()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
