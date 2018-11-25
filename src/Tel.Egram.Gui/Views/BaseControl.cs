2b964e5f5ab96b77cbb895f31c227b1581b4ee3b
Sergey Khabibullin <sergey@khabibullin.com>
2018-11-25 13:48:23 +0300

    Workaround for IActivatable

--- src/Tel.Egram.Gui/Views/BaseControl.cs
+++ src/Tel.Egram.Gui/Views/BaseControl.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Reactive.Disposables;
 using Avalonia;
 using Avalonia.LogicalTree;
 using ReactiveUI;
@@ -5,33 +7,17 @@ using ReactiveUI;
 namespace Tel.Egram.Gui.Views
 {
     /// <summary>
-    /// Hack around not working WhenActivated
+    /// Workaround for model activation
     /// </summary>
     public class BaseControl<TViewModel> : ReactiveUserControl<TViewModel>
         where TViewModel : class
     {
-        private object _dataContext;
-        
         public BaseControl()
         {
-            DataContextChanged += (sender, args) =>
-            {   
-                (_dataContext as ISupportsActivation)?.Activator?.Deactivate();
-                (DataContext as ISupportsActivation)?.Activator?.Activate();
-                _dataContext = DataContext;
-            };
-        }
-
-        protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
-        {
-            base.OnAttachedToLogicalTree(e);
-            (DataContext as ISupportsActivation)?.Activator?.Activate();
-        }
-
-        protected override void OnDetachedFromLogicalTree(LogicalTreeAttachmentEventArgs e)
-        {
-            (DataContext as ISupportsActivation)?.Activator?.Deactivate(true);
-            base.OnDetachedFromLogicalTree(e);
+            this.WhenActivated(disposables =>
+            {
+                Disposable.Create(() => { }).DisposeWith(disposables);
+            });
         }
     }
 }
--- src/Tel.Egram.Gui/Views/BaseWindow.cs
+++ src/Tel.Egram.Gui/Views/BaseWindow.cs
@@ -1,24 +1,22 @@
+using System;
+using System.Reactive.Disposables;
 using Avalonia;
 using ReactiveUI;
 
 namespace Tel.Egram.Gui.Views
 {
     /// <summary>
-    /// Hack around not working WhenActivated
+    /// Workaround for model activation
     /// </summary>
     public class BaseWindow<TViewModel> : ReactiveWindow<TViewModel>
         where TViewModel : class
-    {
-        private object _dataContext;
-        
+    {   
         public BaseWindow()
         {
-            DataContextChanged += (sender, args) =>
-            {   
-                (_dataContext as ISupportsActivation)?.Activator?.Deactivate();
-                (DataContext as ISupportsActivation)?.Activator?.Activate();
-                _dataContext = DataContext;
-            };
+            this.WhenActivated(disposables =>
+            {
+                Disposable.Create(() => { }).DisposeWith(disposables);
+            });
         }
     }
 }
